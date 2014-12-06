bf7d4da7574f5e68211029d5984a10a4e2630495
not--p <lakona808@gmail.com>
2014-12-06 11:08:09 -1000

    Outlining Project
    Added some of the required java methods; removed the unnecessary code
    from temp_convert.c.

--- ics212/10/Hw10.java
+++ ics212/10/Hw10.java
@@ -8,12 +8,13 @@ public class Hw10
     {
         int max_temp = -1;
         
-        while (max_temp < 0) {
+        while (max_temp <= 0) {
             max_temp = get_user_input();
         }
         
         System.out.println("Got " + max_temp);
     }
+
 
     public static int get_user_input()
     {
@@ -22,13 +23,29 @@ public class Hw10
 
         
         try {
-            System.out.println("Give me an int");
+            System.out.println("Enter max temperature [integer > 0]");
             input = kb.nextInt();
         }
         catch (InputMismatchException ime) {
-            System.err.println("Invalid Input " + ime.getMessage());
+            System.err.println("\nInvalid Input");
+        }
+        catch (IllegalStateException ise) {
+            System.err.println("Input/output failure.");
+            System.exit(1);
+        }
+
+        if (input <= 0) {
+            printMaxTempSpec();
+            input = -1;
         }
 
         return input;
+    }
+
+
+    public static void printMaxTempSpec()
+    {
+            System.out.println("Max temperature must be a positive integer " +
+                    "( < 2147483648 )\n");
     }
 }
--- ics212/10/temp_convert.c
+++ ics212/10/temp_convert.c
@@ -15,125 +15,10 @@
 // DESCRIPTION: A program that converts between celcius and fahrenheit.
 //
 ***********************************************************************/
-
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <math.h>
-
-#define INCREMENT 5
-#define COLWIDTH 10
-#define INPUTLIMIT 79
 #define ROUND 5
 
-void print_menu();
-void get_input(char[]);
-int validate(char input[]);
 int round_up(int);
-void print_conversion_table(int max_temp);
-double convertftoC(int fahrenheit);
-void clear_input_buff();
-
-
-int main()
-{
-    char user_input[INPUTLIMIT];
-    int fahrenheit_temp;
-
-    do
-    {
-        get_input(user_input);
-    }
-    while (!validate(user_input));
-
-    sscanf(user_input, "%d", &fahrenheit_temp);
-    print_conversion_table(round_up(fahrenheit_temp));
-
-    return 1;
-}
-
-
-/*****************************************************************
- *
- *  Function name:      get_input
- *
- *  DESCRIPTION:        Calls the menu and caputres user input.
- *
- *  Parameters:         input (char[]) : Storage for user input.
- *
- *  Return values:      void
- *
- ****************************************************************/
-void get_input(char input[])
-{
-    print_menu();
-    scanf("%s", input);
-}
-
-
-/*****************************************************************
- *
- *  Function name:      print_menu 
- *
- *  DESCRIPTION:        Prints a menu to prompt the user for
- *                      input.
- *
- *  Parameters:         
- *
- *  Return values:       void
- *
- ****************************************************************/
-void print_menu()
-{
-    char m0[] = "To display a conversion table for Fahrenheit to Celcius,";
-    char m1[] = "Please enter a maximum temperature in Fahrenheit (F): ";
-    printf("%s\n%s", m0, m1);
-}
-
-
-int validate(char input[])
-/*****************************************************************
- *
- *  Function name:      validate
- *
- *  DESCRIPTION:        Parces the user input.
- *
- *  Parameters:         input (char[]) : Contains user input. 
- *
- *  Return values:       0 : success
- *                      -1 : the value was not found
- *
- ****************************************************************/
-{
-    int valid = 1;
-    int count = 0;
-
-    while (input[count] != '\0')
-    {
-        char current_char = input[count];
-        if (!iscntrl(current_char) && (!isdigit(current_char)))
-        {
-            input[count] = 0;
-            valid = 0;
-        }
-        count++;
-    }
-
-    if (valid && atoi(input) == 0)
-    {
-        valid = 0;
-    }
-
-    if (!valid) 
-    {
-        char m0[] = "! YOUR INPUT CONTAINS INVALID CHARACTERS !";
-        char m1[] = "Valid (F) temperatures can only be integers > 0";
-        printf("\n%s\n%s\n\n", m0, m1);
-    }
-
-    clear_input_buff();
-    return valid;
-}
+float convertftoC(int fahrenheit);
 
 
 /*****************************************************************
@@ -156,28 +41,9 @@ int round_up(int temp)
 }
 
 
-void print_conversion_table(int max_temp)
-{
-    int current_temp = 0;
-    printf("%10s%*s\n", "Fahrenheit", COLWIDTH, "Celcius");
-    while (current_temp <= max_temp) 
-    {
-        printf("%*d%*.2f\n", COLWIDTH, current_temp,
-                COLWIDTH, convertftoC(current_temp));
-        current_temp = current_temp + INCREMENT;
-    }
-}
-
-
-double convertftoC(int fahrenheit)
+float convertftoC(int fahrenheit)
 {
     double celcius;
     celcius = 5 * ((double)fahrenheit - 32) / 9;
     return celcius;
-}
-
-
-void clear_input_buff()
-{
-    while ( getchar() != '\n' );
 }
